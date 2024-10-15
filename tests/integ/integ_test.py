e51581bfa9bbd2a12ac4676e64ad3ddc2b89a623
OverFitted <kozak.bor@gmail.com>
2024-10-15 14:41:34 +0300

    Refactor: ruff check fix

--- tests/integ/integ_test.py
+++ tests/integ/integ_test.py
@@ -1,14 +1,15 @@
 import pytest
 import rollbar
-from app import main
-from app import setup_rollbar
+
+from app import main, setup_rollbar
+
 
 def test_main():
     res = main.main()
     assert res == "Hello, world!"
 
+
 def test_rollbar():
     with pytest.raises(rollbar.ApiError) as e:
         setup_rollbar.main()
         assert e.value == "No token =/"
-        
