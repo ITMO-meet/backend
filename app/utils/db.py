f967338fad140c702a5160b14a1c4ecc659824ee
graevsky <graevsky@bk.ru>
2024-10-28 17:22:10 +0300

    Add db connection x2

---
+++ app/utils/db.py
@@ -0,0 +1,21 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+class Database:
+    def __init__(self):
+        mongo_uri = os.getenv("MONGO_URI")
+        if not mongo_uri:
+            raise ValueError("MONGO_URI not found in env")
+        
+        self.client = AsyncIOMotorClient(mongo_uri)
+        self.db = self.client["meet"]
+
+    def get_collection(self, collection_name):
+        return self.db[collection_name]
+    
+db_instance = Database()
