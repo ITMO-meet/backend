846e726fcd8bdd6eb17ae45daf110aaf0efa5242
graevsky <graevsky@bk.ru>
2024-11-05 15:21:00 +0300

    Add logo and carousel upload

--- app/api/register.py
+++ app/api/register.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, UploadFile, File
 
 from app.utils.db import db_instance
 from app.models.tag import TagSelectionModel
 from app.models.profileDetails import ProfileDetailsModel
+from uuid import uuid4
+from typing import List
 
 router = APIRouter()
 
@@ -30,6 +32,63 @@ async def select_tags(payload: TagSelectionModel):
         )
 
     return {"message": "Tags selected successfully, proceed to the next step"}
+
+
+@router.post("/register/upload_logo")
+async def upload_logo(isu: int, file: UploadFile = File(...)):
+    user_collection = db_instance.get_collection("users")
+
+    file_extension = file.filename.split(".")[-1]
+    filename = f"logos/{isu}_{uuid4()}.{file_extension}"
+
+    file_url = db_instance.upload_file_to_minio(
+        file.file,
+        filename,
+        content_type=file.content_type or "application/octet-stream",
+    )
+
+    update_result = await user_collection.update_one(
+        {"isu": isu}, {"$set": {"photos.logo": file_url}}
+    )
+
+    if update_result.modified_count == 0:
+        raise HTTPException(
+            status_code=404, detail="User not found or logo not uploaded"
+        )
+    return {"message": "Avatar uploaded successfully", "avatar_url": file_url}
+
+
+@router.post("/register/upload_carousel")
+async def upload_carousel(isu: int, files: List[UploadFile] = File(...)):
+    user_collection = db_instance.get_collection("users")
+
+    carousel_urls = []
+
+    for file in files:
+        file_extension = file.filename.split(".")[-1]
+        filename = f"carousel/{isu}_{uuid4()}.{file_extension}"
+
+        file_url = db_instance.upload_file_to_minio(
+            file.file,
+            filename,
+            content_type=file.content_type or "application/octet-stream",
+        )
+
+        carousel_urls.append(file_url)
+
+    update_result = await user_collection.update_one(
+        {"isu": isu}, {"$set": {"photos.carousel": carousel_urls}}
+    )
+
+    if update_result.modified_count == 0:
+        raise HTTPException(
+            status_code=404, detail="User not found or carousel photos not updated"
+        )
+
+    return {
+        "message": "Carousel photos uploaded successfully",
+        "carousel_urls": carousel_urls,
+    }
 
 
 @router.post("/register/profile_details")
--- app/utils/db.py
+++ app/utils/db.py
@@ -2,6 +2,7 @@ from typing import List, Dict, Any
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from dotenv import load_dotenv
+from minio import Minio
 
 load_dotenv()
 
@@ -15,6 +16,35 @@ class Database:
         self.client = AsyncIOMotorClient(mongo_uri)
         self.db = self.client["meet"]
 
+        minio_endpoint = os.getenv("MINIO_ENDPOINT")
+        minio_access_key = os.getenv("MINIO_ACCESS_KEY")
+        minio_secret_key = os.getenv("MINIO_SECRET_KEY")
+        self.minio_bucket_name = os.getenv("MINIO_BUCKET_NAME")
+        minio_use_ssl = os.getenv("MINIO_USE_SSL", "False").lower() == "true"
+
+        if not all(
+            [minio_endpoint, minio_access_key, minio_secret_key, self.minio_bucket_name]
+        ):
+            raise ValueError("MINIO not found in env")
+
+        self.minio_instance = Minio(
+            minio_endpoint, minio_access_key, minio_secret_key, secure=minio_use_ssl
+        )
+
+        if not self.minio_instance.bucket_exists(self.minio_bucket_name):
+            self.minio_instance.make_bucket(self.minio_bucket_name)
+
+    def upload_file_to_minio(self, data, filename, content_type):
+        self.minio_instance.put_object(
+            self.minio_bucket_name,
+            filename,
+            data,
+            length=-1,
+            part_size=10 * 1024 * 1024,
+            content_type=content_type,
+        )
+        return f"{self.minio_bucket_name}/{filename}"
+
     def get_collection(self, collection_name):
         return self.db[collection_name]
 
@@ -25,8 +55,6 @@ class Database:
     async def add_test_tags(self, tags: List[Dict[str, Any]]):
         result = await self.db["tags"].insert_many(tags)
         return [str(tag_id) for tag_id in result.inserted_ids]
-
-
 
 
 db_instance = Database()
