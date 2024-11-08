372dd99ef12c85452e964be6cf540929a5f595a3
graevsky <graevsky@bk.ru>
2024-11-08 14:20:02 +0300

    Feat: get profile details

---
+++ app/api/profile.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter, HTTPException
+from app.utils.db import db_instance
+from bson import ObjectId
+from datetime import timedelta
+
+router = APIRouter()
+
+@router.get("/get_profile/{isu}")
+async def get_profile(isu: int):
+    user_collection = db_instance.get_collection("users")
+    tags_collection = db_instance.get_collection("tags")
+
+    user = await user_collection.find_one({"isu": isu})
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    tag_ids = [ObjectId(tag_id) for tag_id in user.get("tags", [])]
+    preference_ids = [ObjectId(pref_id) for pref_id in user["preferences"].get("relationship_preference", [])]
+
+    user_tags = await tags_collection.find({"_id": {"$in": tag_ids}}).to_list(length=None)
+    user_preferences = await tags_collection.find({"_id": {"$in": preference_ids}}).to_list(length=None)
+    
+    tags = [tag["name"] for tag in user_tags]
+    preferences = [pref["name"] for pref in user_preferences]
+
+    logo_url = None
+    if "logo" in user["photos"]:
+        logo_url = db_instance.minio_instance.presigned_get_object(
+            db_instance.minio_bucket_name, user["photos"]["logo"], expires=timedelta(seconds=3600)
+        )
+    
+    carousel_urls = [
+        db_instance.minio_instance.presigned_get_object(
+            db_instance.minio_bucket_name, photo, expires=timedelta(seconds=3600)
+        ) for photo in user["photos"].get("carousel", [])
+    ]
+
+    profile_data = {
+        "username": user["username"],
+        "bio": user.get("bio", ""),
+        "tags": tags,
+        "preferences": preferences,
+        "photos": {
+            "logo": logo_url,
+            "carousel": carousel_urls
+        },
+        "person_params": {
+            "zodiac_sign": user["person_params"].get("zodiac_sign"),
+            "height": user["person_params"].get("height"),
+            "weight": user["person_params"].get("weight")
+        }
+    }
+    
+    return {"profile": profile_data}
