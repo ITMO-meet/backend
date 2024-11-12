2693538e3b32c870ec2fb09c74511abdde8a3ddb
graevsky <graevsky@bk.ru>
2024-11-13 01:07:24 +0300

    Add tests

--- app/api/chats.py
+++ app/api/chats.py
@@ -11,7 +11,7 @@ router = APIRouter()
 @rollbar_handler
 async def create_chat(payload: CreateChat):
     if payload.isu_1 == payload.isu_2:
-        raise HTTPException(status_code=400, detail="Chat cannot be created for the same user")
+        raise HTTPException(status_code=400, detail="chat cannot be created for the same user")
 
     chat_id = str(uuid4())
     await db_instance.create_chat(chat_id=chat_id, isu_1=payload.isu_1, isu_2=payload.isu_2)
