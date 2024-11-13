0c7c1efa8d493e64e1816e94ad53c443fd9b848a
graevsky <graevsky@bk.ru>
2024-11-13 16:08:21 +0300

    Feat: get story

--- app/api/stories.py
+++ app/api/stories.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from datetime import datetime, timedelta
 from app.utils.db import db_instance
 from app.setup_rollbar import rollbar_handler
-from app.models.chat import CreateChat, SendMessage
+from app.models.story import ViewStory
 
 router = APIRouter()
 
@@ -36,4 +36,14 @@ async def upload_story(isu: int, file: UploadFile = File(...)):
     
     return {"expDate": expiration_date, "id": update_result.updated_id}
 
+@router.get("/get_story/{isu}")
+@rollbar_handler
+async def get_stroy(isu: int):
+    stories_collection = db_instance.get_collection("stories")
     
+    result = await stories_collection.find_one(
+        {"isu": isu}
+    )
+    if not result:
+        raise HTTPException(status_code=404, detail="Story not found")
+    return {"id": result["_id"]}
