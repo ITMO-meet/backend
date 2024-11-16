96454a6ee82828f8431d1581a73e7fd31f90c927
graevsky <graevsky@bk.ru>
2024-11-16 15:09:20 +0300

    Update logic, fix lint

--- app/api/stories.py
+++ app/api/stories.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, HTTPException, Query, File, UploadFile
+from fastapi import APIRouter, HTTPException, File, UploadFile
 from uuid import uuid4
 from datetime import datetime, timedelta
 from app.utils.db import db_instance
 from app.setup_rollbar import rollbar_handler
 from app.models.story import GetStory
+from bson import ObjectId
 
 router = APIRouter()
 
@@ -37,12 +38,31 @@ async def create_story(isu: int, file: UploadFile = File(...)):
 
 @router.get("/get_story/")
 @rollbar_handler
-async def get_stroy(payload: GetStory):
+async def get_story(payload: GetStory):
     stories_collection = db_instance.get_collection("stories")
-    has_access = True # TODO: add check with match
-    result = await stories_collection.find_one(
-        {"isu": payload.isu_whose}
-    )
-    if not result:
+    has_access = True  # TODO: Implement access control logic
+    
+    if not has_access:
+        raise HTTPException(status_code=403, detail="Access denied")
+    
+    story = await stories_collection.find_one({"_id": ObjectId(payload.story_id)})
+    if not story:
         raise HTTPException(status_code=404, detail="Story not found")
-    return {"id": result["_id"]}
+    return {
+        "id": str(story["_id"]),
+        "isu": story["isu"],
+        "url": story["url"],
+        "expiration_date": story["expiration_date"]
+    }
+
+
+@router.get("/get_user_stories/{isu}")
+@rollbar_handler
+async def get_user_stories(isu: int):
+    stories_collection = db_instance.get_collection("stories")
+    cursor = stories_collection.find({"isu": isu})
+    stories = await cursor.to_list(length=None)
+    if not stories:
+        raise HTTPException(status_code=404, detail="User has no stories")
+    story_ids = [str(story['_id']) for story in stories]
+    return {"stories": story_ids}
