e9fbddd8fd2a9ecc6d13c4423a414e99d1d9e3b7
artemijr <Arrtemij96@gmail.com>
2024-12-10 04:19:24 +0300

    Added cors config for communication

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app import setup_rollbar
 from app.api import auth, chats, db, matches, profile, quizes, quizes_results, register, stories, tags
@@ -6,6 +7,14 @@ from app.api import auth, chats, db, matches, profile, quizes, quizes_results, r
 app = FastAPI()
 setup_rollbar.init_rollbar()
 app.include_router(tags.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3070"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(quizes.router, prefix="/tests")
 app.include_router(quizes_results.router, prefix="/results")
 app.include_router(auth.router, prefix="/auth")
