b0efaeab6e7e4ebc88645ccba0ca92a0b5b5a6bb
graevsky <graevsky@bk.ru>
2024-12-11 12:49:41 +0300

    Update tags format

--- app/api/tags.py
+++ app/api/tags.py
@@ -10,10 +10,15 @@ router = APIRouter()
 @rollbar_handler
 async def get_tags():
     tags = await db_instance.get_available_tags()
-    return {"tags": tags}
+    formatted_tags = [{"text": tag["name"], "icon": "tag"} for tag in tags]
+    return {"tags": formatted_tags}
 
 @router.get("/preferences")
 @rollbar_handler
 async def get_preferences():
     preferences = await db_instance.get_special_tags()
-    return {"preferences": preferences}
+    formatted_preferences = [
+        {"text": preference["name"], "icon": "relationship_preferences"}
+        for preference in preferences
+    ]
+    return {"preferences": formatted_preferences}
